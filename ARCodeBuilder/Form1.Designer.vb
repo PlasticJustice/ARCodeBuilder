f6a3118f77d20dfd8c6e71286922b99b3cfcfb62
Regnum <nkochen@hotmail.com>
2018-05-12 20:43:19 -0500

    First Commit

---
+++ ARCodeBuilder/Form1.Designer.vb
@@ -0,0 +1,91 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
+        Me.F2A = New System.Windows.Forms.Label()
+        Me.F2V = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RichTextBox1.Location = New System.Drawing.Point(258, 12)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(264, 426)
+        Me.RichTextBox1.TabIndex = 0
+        Me.RichTextBox1.Text = ""
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(76, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Write 32bit"
+        Me.ToolTip1.SetToolTip(Me.Button1, "0XXXXXXX YYYYYYYY" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Writes the value of Y to:" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "(XXXXXXX + 'offset')")
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'F2A
+        '
+        Me.F2A.AutoSize = True
+        Me.F2A.Location = New System.Drawing.Point(264, 436)
+        Me.F2A.Name = "F2A"
+        Me.F2A.Size = New System.Drawing.Size(39, 13)
+        Me.F2A.TabIndex = 2
+        Me.F2A.Text = "Label1"
+        '
+        'F2V
+        '
+        Me.F2V.AutoSize = True
+        Me.F2V.Location = New System.Drawing.Point(392, 436)
+        Me.F2V.Name = "F2V"
+        Me.F2V.Size = New System.Drawing.Size(39, 13)
+        Me.F2V.TabIndex = 3
+        Me.F2V.Text = "Label1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.F2V)
+        Me.Controls.Add(Me.F2A)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ToolTip1 As ToolTip
+    Friend WithEvents F2A As Label
+    Friend WithEvents F2V As Label
+End Class
---
+++ ARCodeBuilder/Form2.vb
@@ -0,0 +1,39 @@
+﻿Public Class Form2
+    Private Sub TextBox1_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles TextBox1.KeyPress
+
+        '97 - 122 = Ascii codes for simple letters
+        '65 - 90  = Ascii codes for capital letters
+        '48 - 57  = Ascii codes for numbers
+
+        If Asc(e.KeyChar) <> 8 Then
+            If Asc(e.KeyChar) < 48 Or Asc(e.KeyChar) > 57 Then
+                If Asc(e.KeyChar) < 65 Or Asc(e.KeyChar) > 70 Then
+                    If Asc(e.KeyChar) < 97 Or Asc(e.KeyChar) > 102 Then
+                        e.Handled = True
+                    End If
+                End If
+            End If
+        End If
+    End Sub
+    Private Sub TextBox2_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles TextBox2.KeyPress
+
+        '97 - 122 = Ascii codes for simple letters
+        '65 - 90  = Ascii codes for capital letters
+        '48 - 57  = Ascii codes for numbers
+
+        If Asc(e.KeyChar) <> 8 Then
+            If Asc(e.KeyChar) < 48 Or Asc(e.KeyChar) > 57 Then
+                If Asc(e.KeyChar) < 65 Or Asc(e.KeyChar) > 70 Then
+                    If Asc(e.KeyChar) < 97 Or Asc(e.KeyChar) > 102 Then
+                        e.Handled = True
+                    End If
+                End If
+            End If
+        End If
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Form1.F2A.Text = TextBox1.Text
+        Form1.F2V.Text = TextBox2.Text
+        Close()
+    End Sub
+End Class
